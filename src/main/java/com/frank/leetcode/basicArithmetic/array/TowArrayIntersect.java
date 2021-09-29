3e7e75f4a0e222b5243c8c337ddff253ec658eee
zuyafan <710620070@qq.com>
2021-09-29 19:46:17 +0800

    两个数组求交集

--- src/main/java/com/frank/leetcode/basicArithmetic/array/TowArrayIntersect.java
+++ src/main/java/com/frank/leetcode/basicArithmetic/array/TowArrayIntersect.java
@@ -1,6 +1,7 @@
 package com.frank.leetcode.basicArithmetic.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,6 +14,8 @@ import java.util.List;
  *
  * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
  * 输出: [2,2]
+ *
+ *
  * 示例 2:
  *
  * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
@@ -30,19 +33,32 @@ import java.util.List;
 public class TowArrayIntersect {
     public static void main(String[] args) {
 
-        int[] nums1 = {1,2,2,1};
-        int[] nums2 = {2,2};
-
-        intersect(nums1,nums2);
-
+//        int[] nums1 = {1,2,2,1};
+//        int[] nums2 = {2};
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+//        int[] nums1 = {3,1,2};
+//        int[] nums2 = {1,1};
+        int[] intersect = intersect(nums1, nums2);
+        System.out.println(Arrays.toString(intersect));
     }
     public static int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < nums1.length; i++) {
-
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i =0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length){
+            if (nums1[i] == nums2[j]){
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }else if (nums1[i] < nums2[j]){
+                i++;
+            }else {
+                j++;
+            }
         }
-
-
-        return nums1;
+        return list.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
